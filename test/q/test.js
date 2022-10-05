e84eee9fb977d1d82d0d9de4ffcbb8b19994cb69
dinarka <94147914+dinarakh@users.noreply.github.com>
2022-10-06 00:06:03 +0500

    Add files via upload

---
+++ test/q/test.js
@@ -0,0 +1,68 @@
+
+function getBTC(){
+    zxc();
+    zxc1();
+}
+function zxc(){
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then(res => res.json())
+    .then(data => 
+       // document.getElementById('chop-chop').innerHTML = (Object(data.bpi.USD.rate))
+        console.log(Object(data))
+       
+        ).catch(erorr =>{
+            console.log(erorr)
+        })
+}
+
+function zxc1(){
+        fetch("https://api.coindesk.com/v1/bpi/historical/close.json",
+        {
+            "start": "2022-10-03",
+            "end": "2022-10-04"
+        },
+        )
+        data = JSON.parse(data);
+        console.log(data);
+
+        
+       // fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+//         .then(res => res.json())
+//         .then(data => 
+           
+//            console.log(Object(data)),
+    
+//             ).catch(erorr =>{
+//                console.log(erorr)
+//            })
+ }
+
+
+// function getBTC(){
+//     getRate();
+//     getHistoryRate();
+// }
+
+// function getRate(){
+//     $.get("https://api.coindesk.com/v1/bpi/currentprice.json",
+//         function(data){
+//             data = JSON.parse(data);
+//             console.log(data)}
+//             );
+// }
+
+// function getHistoryRate(){
+//     $.get ("https://api.coindesk.com/v1/bpi/historical/close.json",
+//     {
+//         "start" : "2022-10-03",
+//         "end" : "2022-10-04"
+//     },
+//     function(data){
+//         data = JSON.parse(data);
+//         console.log(data);
+//     }
+//     )
+// }
+
+
+
